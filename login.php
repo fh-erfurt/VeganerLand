250a1a9a2f3242df51ae8f612f19f278070b8822
Molham Al-khodari <73823402+Molham321@users.noreply.github.com>
2020-11-28 00:45:21 +0100

    Add files via upload

---
+++ login.php
@@ -0,0 +1,27 @@
+<div class="overlay">
+<form>
+   <div class="con">
+   <header class="head-form">
+      <h2>Log In</h2>
+      <p>Melden Sie sich hier mit Ihrem Benutzernamen und Passwort an</p>
+   </header>
+   <br>
+   <div class="field-set">
+         <input class="form-input" id="txt-input" type="text" placeholder="E-mail" required>
+      <br>
+      <!--Password Input-->
+      <input class="form-input" type="password" placeholder="Password" id="pwd"  name="password" required>
+      <br>
+<!--button LogIn -->
+      <button class="log-in"> Log In </button>
+   </div>
+<!--other buttons -->
+   <div class="other">
+<!--      Forgot Password button-->
+      <button class="btn submits frgt-pass">Passwort vergessen</button>
+<!--     Sign Up button -->
+      <button class="btn submits sign-up">Registrieren</button>
+   </div>
+  </div>
+</form>
+</div>
