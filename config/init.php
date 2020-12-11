1a7e0e236f9d58eb78da7b706baacd35479635cb
Molham Al-khodari <73823402+Molham321@users.noreply.github.com>
2020-12-11 14:17:15 +0100

    Add files via upload

---
+++ config/init.php
@@ -0,0 +1,20 @@
+<?php
+
+define('COREPATH',  'core'.DIRECTORY_SEPARATOR);
+define('VIEWSPATH',  'views'.DIRECTORY_SEPARATOR);
+define('CONTROLLERSPATH',  'controllers'.DIRECTORY_SEPARATOR);
+define('MODELSPATH',  'models'.DIRECTORY_SEPARATOR);
+define('TAMPLATESPATH', 'templates'.DIRECTORY_SEPARATOR);
+define('FUNCTIONSPATH', 'functions'.DIRECTORY_SEPARATOR);
+define('STYLESPATH', 'styles'.DIRECTORY_SEPARATOR);
+
+
+define('ROOTPATH', strlen(dirname($_SERVER['SCRIPT_NAME']))) > 1 ? dirname($_SERVER['SCRIPT_NAME']). '/' : '/';
+
+
+// Include Navbar On All Pages Expect The One With $noNavbar Vairable
+
+if(!isset($noNavbar))
+{
+    include $tpl . 'navbar.php';
+}
