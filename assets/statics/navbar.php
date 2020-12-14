9bb8b77920b9764fc1b5a714891f03453947a9d8
Molham Al-khodari <73823402+Molham321@users.noreply.github.com>
2020-12-14 16:42:38 +0100

    Update navbar.php

--- assets/statics/navbar.php
+++ assets/statics/navbar.php
@@ -1,21 +1,34 @@
 <link rel="stylesheet" href="../../assets/styles/navbarStyle.css">
-<ul> -->
-  <li><a href="#Homepage"><img src="../../assets/images/logo.png" alt="logo" width="50px" height="50px"></a></li>
-  <li><a class="active" href="#home">Startseite</a></li>
-  <li><a href="#news">Angeboten</a></li>
-  <li><a href="#contact">Über uns</a></li>
-  <li><a href="#about">Seite1</a></li>
-  <li><a href="#about">Seite2</a></li>
-  <li><a href="#about">Seite3</a></li>
-  <li><a href="#about">Seite4</a></li>
-  <li><a href="#about">Seite5</a></li>
-  <li><a href="#about">Seite6</a></li>
-  <li style="float:right" class="dropdown">
-    <a href="javascript:void(0)" class="dropbtn">Konto</a>
-    <div class="dropdown-content">
-      <a href="../../views/pages/login.php">Login</a>
-      <a href="#">Einstellungen</a>
-      <a href="../../views/pages/logout.php">Logout</a>
-    </div>
-  </li>
-</ul>
+<header>
+  <h1>Veganer Land</h1>
+</header>
+<nav >
+  <ul> 
+    <!-- <li><a href="#Homepage"><img src="../../assets/images/logo.png" alt="logo" width="50px" height="50px"></a></li> -->
+    <li><a class="active" href="#home">Startseite</a></li>
+    <li><a href="#news">Angeboten</a></li>
+    <li><a href="#contact">Über uns</a></li>
+    <li><a href="#about">Seite1</a></li>
+    <li><a href="#about">Seite2</a></li>
+    <li><a href="#about">Seite3</a></li>
+    <li><a href="#about">Seite4</a></li>
+    <li><a href="#about">Seite5</a></li>
+    <li><a href="#about">Seite6</a></li>
+    <li style="float:right" class="dropdown">
+      <a href="javascript:void(0)" class="dropbtn">Konto</a>
+      <div class="dropdown-content">
+        <a href="../../views/pages/login.php">Login</a>
+        <a href="#">Einstellungen</a>
+        <a href="../../views/pages/logout.php">Logout</a>
+      </div>
+    </li>
+    <li>
+      <div class="search-container">
+        <form action="#" name="searchform" method="GET ">
+        <input type="text" id="search" placeholder="Search..." maxlength="50">
+        <input type="submit" name="search" id="search-submit" value>
+        </form>
+      </div>
+    </li>
+  </ul>
+</nav>
