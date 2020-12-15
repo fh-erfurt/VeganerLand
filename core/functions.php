44097d79bebaf2fadfaade771065791eb56070a0
Molham Al-khodari <73823402+Molham321@users.noreply.github.com>
2020-12-15 23:53:05 +0100

    Update functions.php

--- core/functions.php
+++ core/functions.php
@@ -1,18 +1,37 @@
 <?php 
 
-    // Title Function that echo the page Title in case the page
-    echo 'Function <br>';
-    
-    function getTitle() 
-    {
-        global $pageTitle;
+// Molham Al-khodari
+// 15.22.2020
 
-        if(isset($pageTitle))
+    require '../../config/database.php';
+
+        // Title Function that echo the page Title in case the page
+        
+        function getTitle() 
         {
-            echo $pageTitle;
+            global $pageTitle;
+
+            if(isset($pageTitle))
+            {
+                echo $pageTitle;
+            }
+            else
+            {
+                echo 'Default Title';
+            }
         }
-        else
-        {
-            echo 'Default Title';
+
+        // Returns a true if a there are not Registered Users with that email
+        function isEmailAvailable ( $db, $email) {
+            $request =  $db->prepare(" SELECT * 
+                                                FROM customers 
+                                                WHERE eMail = ? ");
+            $request->execute(array($email)); 
+
+            if($request->rowCount() > 0){
+                return false;
+            }
+            else{
+                return true;
+            }
         }
-    }
