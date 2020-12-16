5ad06479f842d140fe50f42c0a1613f8d071b1a9
Molham Al-khodari <73823402+Molham321@users.noreply.github.com>
2020-12-16 13:45:02 +0100

    Update homepage.php

--- views/pages/homepage.php
+++ views/pages/homepage.php
@@ -1,18 +1,29 @@
 <?
-        $pageTitle = "Homepage";
-        include "../../config/init.php";
 
-        session_start();
-        if (isset($_SESSION['email']))
-        {
-                echo 'welcome ' . $_SESSION['email'] . 'This is Homepage';
-        }
-        else
-        {
-                echo 'you are not logged in';
-        }
+//@author Molham Al-khodari
+//@version 1.0.0
+//16.12.2020
+
+$pageTitle = "Homepage";
+require_once '../../static/header.php';
+include "../../config/init.php";
+// include '../../core/functions.php';  in header zu sehen.
+// include '../../static/header.php';   error
+
+session_start();
+if (isset($_SESSION['email']))
+{
+        echo 'welcome your Email: ' . $_SESSION['email'] . ' this is Homepage <br>';
+        echo 'welcome your ID: ' . $_SESSION['custId'] . '<br>';
+}
+else
+{
+        echo 'you are not logged in';
+}
 ?>
 <title>Homepage</title>
 <link rel="stylesheet" type="text/css" href="../../assets/styles/homepageStyle.css">
+
+<?php  include '../../static/footer.php'; ?>
 
     
