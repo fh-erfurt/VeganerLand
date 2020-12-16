437e4df33994a7be38357af534b969433b6c7da6
Molham Al-khodari <73823402+Molham321@users.noreply.github.com>
2020-12-16 16:46:32 +0100

    Update footer.php

--- assets/statics/footer.php
+++ assets/statics/footer.php
@@ -1,6 +1,52 @@
-<div class ="footer">
-    this is footer <br>
-</div>
+
+<!-- 
+    //@author Molham Al-khodari
+    //@version 1.0.0
+ -->
+
+ <link rel="stylesheet" type="text/css" href="../../assets/styles/footerStyle.css">
+
+    <div class ="footer">
+        <div class="inner_footer">
+            <div class="logo_container">
+                <img src="../../assets/images/logo.png" alt="logo"/>
+            </div>
+            <div class="footer_third">
+                <h1>Need Help?</h1>
+                <a href="#">Terms &amp; Conditions</a>
+                <a href="#">Privacy Policy</a>
+            </div>
+            <div class="footer_third">
+                <h1>More</h1>
+                <a href="#">Brouchures</a>
+                <a href="#">Donate</a>
+                <a href="#">Governance</a>
+                <a href="#">Impact Report</a>   
+                <a href="#">Brouchures</a>
+                <a href="#">Donate</a>
+                <a href="#">Governance</a>
+                <a href="#">Impact Report</a>
+            </div>
+            <div class="footer_third">
+                <h1>Follow us</h1>
+                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+
+                <span>
+                    World Merit Registered Charity <br>
+                    Germany & Wales - 1161402 <br>
+                    Unit 223, The Tea Factory <br>
+                    82 Wood Street <br>
+                    Weimar<br>
+                    99423
+                    
+                </span>
+            </div>
+
+        </div>
+    </div>
     
 </body>
 </html>
