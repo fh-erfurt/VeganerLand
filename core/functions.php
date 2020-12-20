6e733979fb73f98aeb0edc10f4db34794b376136
Molham Al-khodari <73823402+Molham321@users.noreply.github.com>
2020-12-20 22:01:32 +0100

    add function redirectHome

--- core/functions.php
+++ core/functions.php
@@ -37,6 +37,39 @@
             }
         }
 
+        /*
+        ** Home Redirect Function v2.0
+        ** This Function Accept Parameters
+        ** $theMessage = Echo this Message [Error | Success]
+        ** $url = the Link you want to redirect to
+        ** $seconds = Seconds befor Redirecting
+        */
+
+        function redirectHome($theMessage, $url = null, $seconds = 3)
+        {
+            if ($url === null) 
+            {
+                $url = '../../views/pages/homepage.php';
+                $link = 'Homepage';
+            }
+            else {
+                if (isset($_SERVER['HTTP_REFERER']) && $_SERVER['HTTP_REFERER'] !== '')
+                {
+                    $url = $_SERVER['HTTP_REFERER'];
+                    $link = 'Previous Page';
+                }
+                else {
+                    $url = '../../views/pages/homepage.php';
+                    $link = 'Homepage'; 
+                }
+            }
+            echo $theMessage;
+            echo "<div class = 'alert alert-info'>You will be Redirected to $link After $seconds Seconds</div>";
+
+            header("refresh:$seconds;url=$url");
+            exit();
+        }
+
         function doesEmailExists($email) {
             $result = Customers::find('email', $email, self::tableName());
             return (!empty($result)) ? true : false;
