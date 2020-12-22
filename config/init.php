47ccdafe6da746eee428e10c5f60e0c4ac0bdfa9
Jess0208 <73823283+Jess0208@users.noreply.github.com>
2020-12-22 10:33:40 +0100

    Update init.php
    Corrected the filepath and added new filepath.

--- config/init.php
+++ config/init.php
@@ -1,12 +1,13 @@
 <?php
 
 define('COREPATH',  'core'.DIRECTORY_SEPARATOR);
+define('CONFIGPATH', 'config'.DIRECTORY_SEPARATOR);
 define('VIEWSPATH',  'views'.DIRECTORY_SEPARATOR);
 define('CONTROLLERSPATH',  'controllers'.DIRECTORY_SEPARATOR);
 define('MODELSPATH',  'models'.DIRECTORY_SEPARATOR);
-define('TAMPLATESPATH', 'templates'.DIRECTORY_SEPARATOR);
+define('TEMPLATESPATH', 'assets/statics'.DIRECTORY_SEPARATOR);
 define('FUNCTIONSPATH', 'functions'.DIRECTORY_SEPARATOR);
-define('STYLESPATH', 'styles'.DIRECTORY_SEPARATOR);
+define('STYLESPATH', 'assets/styles'.DIRECTORY_SEPARATOR);
 
 
 define('ROOTPATH', strlen(dirname($_SERVER['SCRIPT_NAME']))) > 1 ? dirname($_SERVER['SCRIPT_NAME']). '/' : '/';
@@ -17,6 +18,6 @@ define('ROOTPATH', strlen(dirname($_SERVER['SCRIPT_NAME']))) > 1 ? dirname($_SER
 
 if(!isset($noNavbar))
 {
-    include '../../assets/statics/navbar.php';
+    include TEMPLATESPATH.'/navbar.php';
     
 }
