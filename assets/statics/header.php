1367071379cd37bd9bc9676370a63ad20157ba9a
Jess0208 <73823283+Jess0208@users.noreply.github.com>
2020-12-22 14:47:35 +0100

    Update header.php

--- assets/statics/header.php
+++ assets/statics/header.php
@@ -12,21 +12,19 @@
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <title><?php getTitle()?></title>
-            <link rel="stylesheet" href="../../assets/styles/staticStyle.css">
+            <link rel="stylesheet" href="<?=STYLESPATH.'alertStyle.css'?>">
             <? if ($pageTitle == 'Sing Up') : ?> 
-            <link rel='stylesheet' href ='../../assets/styles/registrationStyle.css'>
-            <? elseif ($pageTitle == 'Login'): ?>
-            <link rel='stylesheet' href ='../../assets/styles/loginStyle.css'>
-            <? elseif ($pageTitle == 'Passwort Vergessen'): ?>
-            <link rel='stylesheet' href ='../../assets/styles/loginStyle.css'>
+            <link rel='stylesheet' href ="<?=STYLESPATH.'registrationStyle.css'?>">
+            <? elseif ($pageTitle == 'Login' || $pageTitle == 'Passwort Vergessen'): ?>
+            <link rel='stylesheet' href ="<?=STYLESPATH.'loginStyle.css'?>">
             <? elseif ($pageTitle == 'Homepage'): ?>
-            <link rel='stylesheet' href ='../../assets/styles/homepageStyle.css'>  
+            <link rel='stylesheet' href ="<?=STYLESPATH.'homepageStyle.css'?>">  
             <? elseif ($pageTitle == 'Einstellung'): ?>
-            <link rel="stylesheet" href="../../assets/styles/settingStyle.css">
+            <link rel="stylesheet" href="<?=STYLESPATH.'settingStyle.css'?>">
             <? elseif ($pageTitle == 'Über uns'): ?>
-            <link rel="stylesheet" href="../../assets/styles/aboutStyle.css">
+            <link rel="stylesheet" href="<?=STYLESPATH.'aboutStyle.css'?>">
             <? elseif ($pageTitle == 'Obst' || $pageTitle == 'Gemüse'): ?>
-            <link rel="stylesheet" href="../../assets/styles/fruitStyle.css">
+            <link rel="stylesheet" href="<?=STYLESPATH.'fruitStyle.css'?>">
             <? endif; ?>
             
         </head>
