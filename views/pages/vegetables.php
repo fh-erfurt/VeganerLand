c6f05904de3046b671f3725d1ca12fc4c5472c23
Jess0208 <73823283+Jess0208@users.noreply.github.com>
2020-12-28 13:55:35 +0100

    Update vegetables.php

--- views/pages/vegetables.php
+++ views/pages/vegetables.php
@@ -1,31 +1,3 @@
-<?php 
-    // Molham Alkhodari
-    // 19.12.2020
-    // 00:00 Uhr
-
-    $pageTitle = 'Obst';
-    require_once '../../assets/statics/navbar.php';
-    require_once '../../assets/statics/header.php';
-    require_once '../../config/database.php';
-
-    // $sql = "SELECT prodId, descrip, cat, stdPrice FROM products WHERE cat = 'V'";
-
-    // $result = $db->query($sql);
-?>
-
-    <!-- <div class="block-container">
-        <ul class="cards">
-            <?php // while($row = $result->fetch()): ?>
-            <li class="cards__item"> 
-
-                <div class="col">
-                    <?php // include 'vegetablesCard.php'?>
-                </div>
-            </li>
-            <?php // endwhile; ?>
-        </ul>
-    </div> -->
-
 <div class="block-container">
     <ul class="cards">
         <? $counter = 0;
@@ -33,12 +5,12 @@
             <li class="cards__item"> 
                 <div class="col">
                     <div class="card">
-                        <div class="card-title"><?=$veggies[$counter]['descrip']?></div>  <!--wird aus datenbank gelesen-->
-                        <img class='ard__image card__image--fence' src="http://placekitten.com/271/180" class="card-img-top" alt="Artikal">   <!--Static Bilder-->
+                        <div class="card-title"><?=$veggies[$counter]['descrip']?></div>
+                        <img class='ard__image card__image--fence' src="<?=VEGETABLEPATH.$veggies[$counter]['descrip']?>.jpg" class="card-img-top" alt="Artikel">
                         <div class="card__text">
-                            <p>test product, hier eine tolle lange beschreibung</p>                     <!--wird aus dem datenbank gelesen-->
+                            <p>test product, hier eine tolle lange beschreibung</p>
                             <hr>
-                            <p><?=$veggies[$counter]['stdPrice']?></p>                                                                <!--wird aus dem datenbank gelesen-->
+                            <p><?=$veggies[$counter]['stdPrice']?></p>
                         </div>
                         <div class="card-footer">
                             <button class="btn btn--block card__btn">details</button>
