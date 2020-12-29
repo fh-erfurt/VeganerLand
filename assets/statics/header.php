ef6f1fa5dbeb75a3bee81b1ac5931c5da51f4640
Jess0208 <73823283+Jess0208@users.noreply.github.com>
2020-12-29 16:17:58 +0100

    Update header.php

--- assets/statics/header.php
+++ assets/statics/header.php
@@ -23,7 +23,7 @@
             <link rel="stylesheet" href="<?=STYLESPATH.'settingStyle.css'?>">
             <? elseif ($pageTitle == 'Über uns'): ?>
             <link rel="stylesheet" href="<?=STYLESPATH.'aboutStyle.css'?>">
-            <? elseif ($pageTitle == 'Obst' || $pageTitle == 'Gemüse'): ?>
+            <? elseif ($pageTitle == 'Obst' || $pageTitle == 'Gemüse' || $pageTitle == 'Angebote'): ?>
             <link rel="stylesheet" href="<?=STYLESPATH.'fruitStyle.css'?>">
             <? endif; ?>
             <script type="text/javascript" src="assets/js/forms.js"></script>
