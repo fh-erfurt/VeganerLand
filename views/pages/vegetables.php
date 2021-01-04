f7757ba4e6d00e5be101f5dc05e05b6150fb84cd
Jess0208 <73823283+Jess0208@users.noreply.github.com>
2021-01-04 15:42:14 +0100

    Update vegetables.php
    Same as with fruits.php

--- views/pages/vegetables.php
+++ views/pages/vegetables.php
@@ -1,3 +1,11 @@
-<?//@author Molham Al-Khodari, Jessica Eckardtsberg
-include "include.php";?>
-<link rel="stylesheet" href="<?=STYLESPATH.'navbarStyle.css'?>">
+<?
+//@author Molham Al-Khodari, Jessica Eckardtsberg
+?>
+<nav class="fruit-nav-container">
+    <ul class="fruit-nav-list">
+        <li><a href="?a=vegetables">Alle</a></li>
+        <li><a href="?a=vegetables&cat=potato">Kartoffeln</a></li>
+        <li><a href="?a=vegetables&cat=mushroom">Pilze</a></li>
+    </ul>
+</nav>
+<?include "include.php";?>
