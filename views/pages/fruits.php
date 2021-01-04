6545fd8d605981c47b2910b3ec19b5638d81348d
Jess0208 <73823283+Jess0208@users.noreply.github.com>
2021-01-04 15:41:12 +0100

    Update fruits.php
    The navbar is there again...

--- views/pages/fruits.php
+++ views/pages/fruits.php
@@ -1,3 +1,14 @@
-<?//@author Molham Al-Khodari, Jessica Eckardtsberg
-include "include.php";?>
+<?
+//@author Molham Al-Khodari, Jessica Eckardtsberg
+?>
 <link rel="stylesheet" href="<?=STYLESPATH.'navbarStyle.css'?>">
+<nav class="fruit-nav-container">
+    <ul class="fruit-nav-list">
+        <li><a href="?a=fruits">Alle</a></li>
+        <li><a href="?a=fruits&cat=citrus">Zitrus Früchte</a></li>
+        <li><a href="?a=fruits&cat=berry">Beeren</a></li>
+        <li><a href="?a=fruits&cat=nuts">Nüsse</a></li>
+        <li><a href="?a=fruits&cat=exotics">Exotische Früchte</a></li>
+    </ul>
+</nav>
+<?include "include.php";?>
