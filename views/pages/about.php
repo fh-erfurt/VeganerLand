685c44144c77df4ba857d06c36afe927e6d827f8
Matar1 <56829210+Matar1@users.noreply.github.com>
2021-01-15 08:01:43 +0100

    Add files via upload

--- views/pages/about.php
+++ views/pages/about.php
@@ -119,7 +119,60 @@
             nulla vehicula scelerisque. Sed ut auctor lorem. Ut non tristique nisi. Nulla vitae neque a nisi facilisis aliquam vel in nisl. Donec blandit pulvinar ligula quis vestibulum. Proin at rutrum augue. Aliquam sit amet porttitor tortor.
         </p>
     </article>
-
+    <article id="AGB">
+        <h2>AGB</h2>
+        <p>
+        <h1>AGB</h1>
+                        <hr class="saw-tooth-line"/>
+                        <p>
+                            <strong>Allgemeine Geschäftsbedingungen der Firma Hansen Obst (Claus Hansen e.K) für Bestellungen über die Internetpräsenz www.hansenobst.de</strong>
+                        </p>
+                        <h2>1. Allgemeines, Kundenkreis, Sprache</h2>
+                        <p>(1) Alle Angebote, Kaufverträge, Lieferungen und Dienstleistungen aufgrund von Bestellungen unserer Kunden über unseren Online Shop www.hansenobst.de (nachfolgend der „Webshop“) unterliegen diesen Allgemeinen Geschäftsbedingungen.</p>
+                        <p>(2) Das Produktangebot in unserem Webshop richtet sich gleichermaßen an Verbraucher und Unternehmer, jedoch nur an Endabnehmer. Für Zwecke dieser Allgemeinen Geschäftsbedingungen, ist ein Verbraucher jede natürliche Person, die den Vertrag zu einem Zweck abschließt, der weder ihrer gewerblichen noch ihrer selbständigen beruflichen Tätigkeit zugerechnet werden kann (§ 13 des Bürgerlichen Gesetzbuches – BGB) und ist ein „Unternehmer“ eine natürliche oder juristische Person oder eine rechtsfähige Personengesellschaft, die bei Abschluss des Vertrages in Ausübung ihrer gewerblichen oder selbständigen beruflichen Tätigkeit handelt (§ 14 Abs. 1 BGB).</p>
+                        <p>(3) Geschäftsbedingungen des Kunden finden keine Anwendung, auch wenn wir ihrer Geltung im Einzelfall nicht gesondert widersprechen.</p>
+                        <p>(4) Die Verträge mit dem Kunden werden ausschließlich in deutscher Sprache geschlossen.</p>
+                        <h2>2. Urheberrechte/ Rechte Dritter</h2>
+                        <p>(1) Der Kunde ist für die inhaltliche Richtigkeit der übermittelten Daten verantwortlich. Der Kunde sichert zu, dass er für die übermittelten Bilddateien alle erforderlichen Rechte (Urheber-, Namens-, Markenrechte oder sonstige Rechte) innehat. Zudem versichert der Kunde, dass die übermittelten Bilddateien sowie deren Nutzung keine Rechte Dritter verletzen und nicht gegen das Gesetz verstoßen. Rechte Dritter sind dabei Urheber-, Namens-, Markenrechte oder sonstige Rechte. Sollten dennoch Rechte Dritter verletzt sein, stellt uns der Kunde von sämtlichen Ansprüchen, welche durch Dritte geltend gemacht werden, frei und hat uns die entstehenden Rechtsverfolgungskosten zu erstatten. Dies betrifft insbesondere die Verletzung von Rechten aufgrund der dem erteilten Auftrag entsprechenden Bearbeitung, Nutzung oder Vervielfältigung der vom Kunden an uns übermittelten Bilddateien.</p>
+                        <p>(2) Darüber hinaus sichert der Kunde zu, dass die Inhalte seiner übermittelten Bilddateien gegen keine geltenden Verbotsnormen verstoßen. Wir informieren die zuständigen Strafverfolgungsbehörden, sobald der Kunde gegen diese Zusicherung verstößt.</p>
+                        <h2>3. Vertragsschluss</h2>
+                        <p>(1) Unsere Angebote im Webshop sind unverbindlich.</p>
+                        <p>(2) Durch Aufgabe einer Bestellung im Webshop macht der Kunde ein verbindliches Angebot zum Kauf des betreffenden Produkts. Der Kunde ist an das Angebot bis zum Ablauf des dritten auf den Tag des Angebots folgenden Werktages gebunden.</p>
+                        <p>(3) Wir werden dem Kunden unverzüglich nach Eingang des Angebots eine Bestellbestätigung zusenden, die eine Annahme des Angebots darstellt. Der Kaufvertrag mit dem Kunden kommt mit der Bestellbestätigung zustande.</p>
+                        <p>(4) Jeder Kunde, der Verbraucher ist, ist berechtigt, das Angebot nach Maßgabe der besonderen Widerrufs- und Rückgabebelehrung, die ihm im Rahmen der Bestellung auf unserer Website mitgeteilt wird, zu widerrufen und die Ware zurückzusenden.</p>
+                        <p>(5) Werden wir durch unsere Lieferanten und Zulieferer nicht richtig und/oder rechtzeitig beliefert, können wir vom Vertrag durch Erklärung gegenüber dem Kunden zurücktreten, soweit wir die Nicht- oder Fehllieferung nicht zu vertreten hat. Der Kunde wird über die Nichtverfügbarkeit des Produktes unverzüglich informiert und ihm wird die bereits erbrachte Gegenleistung unverzüglich zurückerstattet. Die gesetzlichen Rechte des Kunden wegen Lieferverzuges werden durch die vorstehende Regelung nicht berührt, wobei der Kunde Schadensersatz nur nach besonderer Maßgabe der Ziffer 9 dieser Allgemeinen Geschäftsbedingungen verlangen kann.</p>
+                        <h2>4. Preise und Zahlung</h2>
+                        <p>(1) Unsere Preise schließen die gesetzliche Umsatzsteuer ein, nicht jedoch Versandkosten. Zölle und ähnliche Abgaben hat der Kunde zu tragen.</p>
+                        <p>(2)&nbsp;In unserem Online-Shop bieten wir die Zahlungsarten auf Rechnung, PayPal und Sofort an. Wir behalten uns das Recht vor, in einzelnen Fällen den Auftraggeber nach der Erstbestellung um Vorkasse zu bitten.</p>
+                        <p>(3) Ist Lieferung auf Rechnung vereinbart, sind unsere Rechnungen innerhalb von 10 Tagen ab Rechnungsdatum beim Kunden zur Zahlung fällig, unabhängig vom Lieferzeitpunkt.</p>
+                        <p>(4) Dem Kunden steht kein Aufrechnungs- oder Zurückbehaltungsrecht zu, soweit nicht die Gegenforderung unbestritten oder rechtskräftig festgestellt ist.</p>
+                        <h2>5. Versendung der Ware</h2>
+                        <p>(1) Soweit in der Artikelbeschreibung keine Lieferzeiten angegeben sind und solange der Kunde kein Wunschlieferdatum angegeben hat, erfolgt die Lieferung bei einer Bestellung bis 15:00 Uhr innerhalb eines Werktages (keine Lieferung an Sonntagen, Montagen und gesetzlichen Feiertagen). Die Frist für die Lieferung beginnt bei Zahlung per Vorkasse am Tag nach Erteilung des Zahlungsauftrags an das überweisende Kreditinstitut bzw. bei anderen Zahlungsarten am Tag nach Vertragsschluss zu laufen und endet mit dem Ablauf des letzten Tages der Frist. Fällt der letzte Tag der Frist auf einen Samstag, Sonntag oder einen am Lieferort staatlich anerkannten allgemeinen Feiertag, so tritt an die Stelle eines solchen Tages der nächste Werktag. Durch die Angabe eines Wunschlieferdatums kommt kein Fixgeschäft zustande.</p>
+                        <p>(2) Werden wir durch unsere Lieferanten und Zulieferer nicht richtig und/oder rechtzeitig beliefert, können wir vom Vertrag durch Erklärung gegenüber dem Kunden zurücktreten, soweit wir die Nicht- oder Fehllieferung nicht zu vertreten haben. Der Kunde wird über die Nichtverfügbarkeit des Produktes unverzüglich informiert und ihm wird die bereits erbrachte Gegenleistung unverzüglich zurückerstattet.</p>
+                        <p>(3) Ist die Ware auf absehbare Zeit nicht bei unseren Lieferanten verfügbar, sind wir zum Rücktritt vom Kaufvertrag berechtigt. Im Falle eines Rücktritts werden wir dem Kunden seine an uns geleisteten Zahlungen unverzüglich erstatten. Die gesetzlichen Rechte des Kunden wegen Lieferverzuges werden durch die vorstehende Regelung nicht berührt, wobei der Kunde Schadenersatz nur nach besonderer Maßgabe der Ziffer 9 dieser Allgemeinen Geschäftsbedingungen verlangen kann.</p>
+                        <p>(4) Wir sind zu Teillieferungen von in einer Bestellung erfassten, getrennt nutzbaren Produkten berechtigt, wobei wir die dadurch verursachten zusätzlichen Versandkosten tragen.</p>
+                        <h2>6. Versand, Versicherung und Gefahrübergang</h2>
+                        <p>(1) Soweit nicht ausdrücklich anders vereinbart, bestimmen wir die angemessene Versandart und das Transportunternehmen nach unserem billigen Ermessen.</p>
+                        <p>(2) Ist der Kunde Verbraucher, geht die Gefahr des zufälligen Untergangs, der zufälligen Beschädigung oder des zufälligen Verlusts der gelieferten Ware in dem Zeitpunkt auf den Kunden über, in dem die Ware an den Kunden ausgeliefert wird oder der Kunde in Annahmeverzug gerät. In allen anderen Fällen geht die Gefahr mit der Auslieferung der Ware an das Transportunternehmen auf den Kunden über.</p>
+                        <p>(3) Wir werden die Ware gegen die üblichen Transportrisiken auf unsere Kosten versichern.</p>
+                        <h2>7. Gewährleistung</h2>
+                        <p>(1) Für Gewährleistungsansprüche gelten die gesetzlichen Regelungen.</p>
+                        <p>(2) Nur gegenüber Unternehmern gilt folgendes: Der Kunde hat die Ware unverzüglich nach Übersendung sorgfältig zu untersuchen. Die gelieferte Ware gilt als vom Kunden genehmigt, wenn ein Mangel uns nicht (i) im Falle von offensichtlichen Mängeln innerhalb von fünf Werktagen nach Lieferung oder sonst (ii) innerhalb von fünf Werktagen nach Entdeckung des Mangels angezeigt wird.</p>
+                        <h2>8. Haftung</h2>
+                        <p>(1) Unsere Haftung für Fahrlässigkeit (ausgenommen grobe Fahrlässigkeit) ist im Fall des Lieferverzugs auf einen Betrag von (10)% des jeweiligen Kaufpreises (einschließlich Umsatzsteuer) begrenzt. Wir haften nicht (gleich, aus welchem Rechtsgrund) für Schäden, die nach Art des jeweiligen Auftrags und der Ware und bei normaler Verwendung der Ware typischerweise nicht zu erwarten sind. Die vorstehenden Haftungsbeschränkungen gelten nicht im Falle von Vorsatz oder grober Fahrlässigkeit.</p>
+                        <p>(2) Die Einschränkungen dieses § 9 gelten nicht für unsere Haftung für garantierte Beschaffenheitsmerkmale iSv. § 444 BGB, wegen Verletzung des Lebens, des Körpers oder der Gesundheit oder nach dem Produkthaftungsgesetz.</p>
+                        <h2>9. Datenschutz</h2>
+                        <p>(1) Wir dürfen die die jeweiligen Kaufverträge betreffenden Daten verarbeiten und speichern, soweit dies für die Ausführung und Abwicklung des Kaufvertrages erforderlich ist und solange wir zur Aufbewahrung dieser Daten aufgrund gesetzlicher Vorschriften verpflichtet sind.</p>
+                        <p>(2) Wir werden personenbezogene Kundendaten nicht ohne das ausdrücklich erklärte Einverständnis des Kunden an Dritte weiterleiten, ausgenommen, soweit wir gesetzlich zur Herausgabe von Daten verpflichtet sind.</p>
+                        <p>(3) Die Erhebung, Übermittlung oder sonstige Verarbeitung von personenbezogenen Daten des Kunden zu anderen als den hier genannten Zwecken ist uns nicht gestattet.</p>
+                        <h2>10. Anwendbares Recht und Gerichtsstand</h2>
+                        <p>(1) Der zwischen uns und dem Kunden bestehende Vertrag unterliegt vorbehaltlich zwingender internationalprivatrechtlicher Vorschriften dem Recht der Bundesrepublik Deutschland.</p>
+                        <p>(2) Ist der Kunde Kaufmann iSd. § 1 Abs. 1 des Handelsgesetzbuches (HGB), eine juristische Person des öffentlichen Rechts oder ein öffentlich-rechtliches Sondervermögen, so sind die Gerichte in Thüringen für alle Streitigkeiten aus oder im Zusammenhang mit dem betreffenden Vertragsverhältnis ausschließlich zuständig. In allen anderen Fällen können wir oder der Kunde Klage vor jedem aufgrund gesetzlicher Vorschriften zuständigen Gericht erheben.</p>
+                        <h2>11. Widerruf, Rücksendekosten bei Widerruf</h2>
+                        <p>Verbraucher haben das Recht, den Vertrag nach Maßgabe der Widerrufsbelehrung zu widerrufen. Verbraucher ist jede natürliche Person, die ein Rechtsgeschäft zu Zwecken abschließt, die überwiegend weder ihrer gewerblichen noch ihrer selbstständigen beruflichen Tätigkeit zugerechnet werden kann. Im Falle des Widerrufs trägt der Kunde die Kosten der Rücksendung.</p>
+                        <p>&nbsp;</p>
+        </p>
+    </article>
     <article id="support">
         <h2>Unterstützung</h2>
         <p>
