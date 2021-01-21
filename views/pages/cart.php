434f9f68a35311402a22a333f92e8a9d22645fc3
Jess0208 <73823283+Jess0208@users.noreply.github.com>
2021-01-21 14:35:52 +0100

    Added delete button

--- views/pages/cart.php
+++ views/pages/cart.php
@@ -4,18 +4,21 @@
             <th>Produkt</th>
             <th>Anzahl</th>
             <th>Preis</th>
+            <td></td>
         </tr>
         <? for ($idx = 0; $idx < count($cart); $idx++) { ?>
         <tr>
             <td><? echo ucfirst($prodInfo[$idx][0]['descrip']) ?></td>
             <td><? echo $cart[$idx]['qyt'] ?></td>
             <td><? echo $price[$idx]." €"; ?></td>
+            <td><form method = "post"><button name="delete" type="submit" value="<?=$cart[$idx]['itemId']?>">X</button></form></td>
         </tr>
             <?        
         }?>
         <tr>
             <th>Gesamt:</th>
             <td colspan="2" style="text-align: right;"><? echo $ttprice." €" ?></td>
+            <td></td>
         </tr>
     </table>
 </div>
