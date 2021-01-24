3beda7e95104dda19f7e542f89d220c6acefde28
Molham Al-khodari <73823402+Molham321@users.noreply.github.com>
2021-01-24 19:13:08 +0100

    Update navbar.php

--- assets/statics/navbar.php
+++ assets/statics/navbar.php
@@ -1,72 +1,56 @@
+<!-- Molham Al-khodari -->
 <link rel="stylesheet" href="<?=STYLESPATH.'navbarStyle.css'?>">
-
-<script
-  src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-  integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8="
-  crossorigin="anonymous">
-</script>
-<script>
-
-$(function() {
-    $(".toggle").on("click", function() {
-        if ($(".item").hasClass("active")) {
-            $(".item").removeClass("active");
-        } else {
-            $(".item").addClass("active");
-        }
-    });
-});
-</script>
-
 </head>
 <nav class="fixed">
-    <ul class="menu">
-        <li class="logo"><a href="?c=pages&a=homepage">VeganerLand</a></li>
-        <li class="item"><a href="?c=pages&a=homepage">Startseite</a></li>
-        <li class="item"><a href="?c=pages&a=about">Über uns</a></li>
-        <li class="item"><a href="?c=pages&a=bargain">Angebote</a></li>
-        <li class="dropdown item"><a href="?c=pages&a=fruits">Obst</a>
-        <div class="dropdown-content">
-          <a href="?c=pages&a=fruits">Alle</a>
-          <a href="?c=pages&a=fruits&cat=citrus">Zitrus Früchte</a>
-          <a href="?c=pages&a=fruits&cat=berry">Beeren</a>
-          <a href="?c=pages&a=fruits&cat=nuts">Nüsse</a>
-          <a href="?c=pages&a=fruits&cat=exotics">Exotische Früchte</a>
-        </div>
-      </li>
-      <li class="dropdown item"><a href="?c=pages&a=vegetables">Gemüse</a>
-        <div class="dropdown-content">
-          <a href="?c=pages&a=vegetables">Alle</a>
-          <a href="?c=pages&a=vegetables&cat=potato">Kartoffeln</a>
-          <a href="?c=pages&a=vegetables&cat=mushroom">Pilze</a>
-        </div>
-      </li>
-        </li>
-        <!-- searchform -->
-        <li class="item">
-          <div class="search-container">
-            <form name="search" method="post">
-              <input type="text" name="search" placeholder="Suche" maxlength="50">
-              <button class="search" type="submit" name="submit" formaction="?c=pages&a=search">Suchen</button>
-            </form>
-          </div>
-        </li>
+  <ul class="menu">
+    <li class="logo"><a href="?c=pages&a=homepage">VeganerLand</a></li>
+    <li class="item"><a href="?c=pages&a=homepage">Startseite</a></li>
+    <li class="item"><a href="?c=pages&a=about">Über uns</a></li>
+    <li class="item"><a href="?c=pages&a=bargain">Angebote</a></li>
 
-      <!-- Konto dropdown -->
-      <?php if(isset($_SESSION['email'])) : ?>
-        <li class="dropdown item button secondary"><a href="#">Konto</a>
-          <div class="dropdown-content">
-            <a href="?c=pages&a=setting">Einstellungen</a>
-            <a href="?c=pages&a=logout">Logout</a>
-          </div>
-        </li>
-      <?php else : ?>
-        <li class="dropdown item button"><a href="?c=pages&a=login">Log In</a></li>
+    <li class="dropdown item"><a href="#">Obst</a>
+      <div class="dropdown-content">
+        <a href="?c=pages&a=fruits">Alle</a>
+        <a href="?c=pages&a=fruits&cat=citrus">Zitrus Früchte</a>
+        <a href="?c=pages&a=fruits&cat=berry">Beeren</a>
+        <a href="?c=pages&a=fruits&cat=nuts">Nüsse</a>
+        <a href="?c=pages&a=fruits&cat=exotics">Exotische Früchte</a>
+      </div>
+    </li>
 
-        <?php endif; ?>
-                <li class="item button secondary"><a id="cart" href="?c=pages&a=cart">Warenkorb()</a></li>
-        <li class="toggle"><span class="bars"></span></li>
-    </ul>
+    <li class="dropdown item"><a href="#">Gemüse</a>
+      <div class="dropdown-content">
+        <a href="?c=pages&a=vegetables">Alle</a>
+        <a href="?c=pages&a=vegetables&cat=potato">Kartoffeln</a>
+        <a href="?c=pages&a=vegetables&cat=mushroom">Pilze</a>
+      </div>
+    </li>
+    
+    <!-- searchform -->
+    <li class="item">
+      <div class="search-container">
+        <form name="search" method="post">
+          <input type="text" name="search" placeholder="Suche" maxlength="50">
+          <button class="search" type="submit" name="submit" formaction="?c=pages&a=search">Suchen</button>
+        </form>
+      </div>
+    </li>
+
+  <!-- Konto dropdown -->
+  <?php if(isset($_SESSION['email'])) : ?>
+    <li class="dropdown item button secondary"><a href="#">Konto</a>
+      <div class="dropdown-content">
+        <a href="?c=pages&a=setting">Einstellungen</a>
+        <a href="?c=pages&a=logout">Logout</a>
+      </div>
+    </li>
+  <?php else : ?>
+    <li class="dropdown item button"><a href="?c=pages&a=login">Log In</a></li>
+  <?php endif; ?>
+
+      <li class="item button secondary"><a id="cart" href="?c=pages&a=cart">Warenkorb()</a></li>
+      <li class="toggle"><span class="bars"></span></li>
+  </ul>
 </nav>
 
 <div id="popover" class="popover" style="width: 100vw; height: 100vw; display: none; background: rgba(0,0,0,0.8); position: absolute; top: 0; left: 0;">
@@ -101,4 +85,23 @@ $(function() {
     }
 
   });
+</script>
+
+<!-- the script is only good for opening the three points (toggle) for Mobile devices -->
+<script
+  src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+  integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8="
+  crossorigin="anonymous">
+</script>
+
+<script>
+$(function() {
+    $(".toggle").on("click", function() {
+        if ($(".item").hasClass("active")) {
+            $(".item").removeClass("active");
+        } else {
+            $(".item").addClass("active");
+        }
+    });
+});
 </script>
