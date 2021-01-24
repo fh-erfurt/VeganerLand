b89ec83f242188ebcc5487a0f80de1d379846e1f
Jess0208 <73823283+Jess0208@users.noreply.github.com>
2021-01-24 11:24:21 +0100

    Update pagesController.php
    actionCart() isn't complete, but the commented insert works.
    Also delted actionSettings() {is in another controller}, actionResetPassword() and actionFavorits() {we don't need the last two.}.

--- controllers/pagesController.php
+++ controllers/pagesController.php
@@ -51,11 +51,6 @@ class PagesController extends Controller {
         }
     }
     
-    public function actionResetPassword() {
-        // Customer as forgotten Password. → Customer gets an E-Mail with a new password.
-        // Customer wants to change Password. → Customer gives old password and 2x new password.
-    }
-    
     public function actionSearch() {
         // Input whta to search in field (Name of fruit of veggie)
         if (isset($_POST['submit'])) {
@@ -70,155 +65,6 @@ class PagesController extends Controller {
 
     public function actionAbout() {
         // This is a static site. So nothing is to do, but we kind off need the method, I think.
-    }
-
-    public function actionSetting() {
-    // Customer can look into his given data and change his address, phone number and favorite products.
-
-        if (isset($_SESSION['email'])) {
-            $info = ['custId'   => null,
-                    'firstName' => null,
-                    'lastName'  => null,
-                    'email'     => null,
-                    'phone'     => null,
-                    'password'  => null,
-                    'addressId' => null,
-                    'street'    => null,
-                    'number'    => null,
-                    'zip'       => null,
-                    'city'      => null];
-            $email = $_SESSION['email'];
-
-            $custInfo = Customers::find("email = '$email'", Customers::tableName());
-            $info['custId'] = $custInfo[0]['custId'];
-            $info['firstName'] = $custInfo[0]['firstName'];
-            $info['lastName'] = $custInfo[0]['lastName'];
-            $info['email'] = $custInfo[0]['email'];
-            $info['phone'] = $custInfo[0]['phone'];
-            $info['password'] = $custInfo[0]['password'];
-
-            $addressId = $custInfo[0]['addressId'];
-            $info['addressId'] = $addressId;
-
-            if (!empty($addressId)) {
-                $addressInfo = Address::find("addressId = '$addressId'", Address::tableName());
-                $info['street'] = $addressInfo[0]['street'];
-                $info['number'] = $addressInfo[0]['number'];
-                $info['zip'] = $addressInfo[0]['zip'];
-                $info['city'] = $addressInfo[0]['city'];
-            }
-
-            $this->setParams('customerInfo', $info);
-
-            if (isset($_POST['submit'])) {
-                $newInfo = ['custId'    => $_POST['custId'],
-                            'email'     => $_POST['email'],
-                            'password'  => $_POST['oldPassword'],
-                            'phone'     => $_POST['phone'],
-                            'addressId' => $_POST['addressId'],
-                            'street'    => $_POST['street'],
-                            'number'    => $_POST['number'],
-                            'zip'       => $_POST['zip'],
-                            'city'      => $_POST['city']];
-                $formErrors = 0;
-                $noNewAddress = false;
-
-                if (!empty($_POST['newPassword'])) {
-                    if (isPasswordSafe($_POST['newPassword'])) {
-                        $newInfo['password'] = md5($_POST['newPassword']);
-                    } else {
-                        $formErrors++;
-                    }
-                }
-
-                if (empty($newInfo['email'])) {
-                    $formErrors++;
-                } else {
-                    // Checks if the email has been changed into another email already existing in the database.
-                    if (doesEmailExists($newInfo['email'])
-                    &&  $newInfo['email'] !== $email) {
-                        $formErrors++;
-                    }
-                }
-
-                // Checks if all fields are filled.
-                if (!empty($newInfo['street'])
-                &&  !empty($newInfo['number'])
-                &&  !empty($newInfo['zip'])
-                &&  !empty($newInfo['city'])){
-
-                    $street = $newInfo['street'];
-                    $number = $newInfo['number'];
-                    $zip = $newInfo['zip'];
-                    $city = $newInfo['city'];
-                    $addressInfo = Address::find("street = '$street' AND
-                                                  number = '$number' AND 
-                                                  zip    = '$zip'    AND 
-                                                  city   = '$city'", 
-                                                  Address::tableName());
-
-                } else if (!empty($newInfo['street'])//Checks if only some fields are filled.
-                       ||  !empty($newInfo['number'])
-                       ||  !empty($newInfo['zip'])
-                       ||  !empty($newInfo['city'])) {
-                    $formErrors++;
-                } else {
-                    $noNewAddress = true;
-                }
-
-                if ($formErrors !== 0) {
-                    echo '<div class="alert alert-danger">Update konnte nicht ausgeführt werden. Angaben waren unvollständig oder unzulässig.</div>';
-                    exit();
-                } else {
-                    // Update the Database
-                    if (empty($addressInfo) && !$noNewAddress){ //Creats an new entry in sddress table if address doesn't exists there.
-                        try {
-                            $sql1 = "INSERT INTO " . Address::tableName() . " (street, number, zip, city) 
-                                     VALUES ('$street', '$number', '$zip', '$city');";
-                            
-                            $stmt = $GLOBALS['db']->prepare($sql1);
-                            $stmt->execute();
-
-                            $newInfo['addressId'] = $GLOBALS['db']->lastInsertId();
-                        } catch (\PDOException $e){
-                            echo 'Fehlschlag: ' . $e->getMessage();
-                        }
-                    } else if (!$noNewAddress){
-                        $newInfo['addressId'] = $addressInfo[0]['addressId'];
-                    }
-
-                    // Update Customer Entry
-                    $email = $newInfo['email'];
-                    $phone = $newInfo['phone'];
-                    $password = $newInfo['password'];
-                    $addressId = $newInfo['addressId'];
-                    $id = $newInfo['custId'];
-
-                    try {
-                        $sql2 = "UPDATE " . Customers::tableName() . " SET 
-                                email = '$email', phone = '$phone', password = '$password', addressId = '$addressId' 
-                                WHERE custId = $id;";
-                        
-                        $stmt = $GLOBALS['db']->prepare($sql2);
-                        $stmt->execute();
-                    } catch (\PDOException $e) {
-                        echo 'Update fehlgeschlagen: ' . $e->getMessage();
-                    }
-                    header('Location: ?c=pages&a=setting');
-                    echo '<div class="alert alert-success">Update erfolgreich!</div>';
-                }
-            }
-        } else {
-            header('Location: ?c=pages&a=homepage');
-            echo '<div class="alert alert-danger">Du bist nicht angemeldet! <a href="?login">Anmelden</a></div>';
-        }
-    }
-    public function actionFavorites() {
-        // Gives a List of the Customers favorite products. We still need a model for this table.
-        $custId = $this->params['userId'];
-        $favs = Favorites::find("customerId = '$custId'", Favorites::tableName());
-
-        $this->setParams('favorites', $favs);
     }
 
     public function actionFruits() {
@@ -277,7 +123,7 @@ class PagesController extends Controller {
         $this->addToFavorites();
     }
 
-        protected function addToCart() {
+   protected function addToCart() {
         if (isset($_SESSION['custId'])) {
             $idC = $_SESSION['custId'];
             if (isset($_POST['submit'])) {
@@ -298,7 +144,15 @@ class PagesController extends Controller {
                             echo 'Update fehlgeschlagen: ' . $e->getMessage();
                         }
                     } else {
-                        // Maybe get back the itemId.
+                        $idI = $check[0]['itemId'];
+                        try {
+                            $sql = "UPDATE " . OrderItems::tableName() . " SET isSend = 'f' WHERE itemId = $idI;";
+                            $stmt = $GLOBALS['db']->prepare($sql);
+                            $stmt->execute();
+                        } catch (\PDOException $e) {
+                            echo '<div class="alert alert-danger">Bestellung fehlgeschlagen.</div>';
+                            echo 'Update fehlgeschlagen: ' . $e->getMessage();
+                        }
                     }
                 } else {
                     echo '<div class="alert alert-danger">Bitte gib die gewünschte Menge an!</div>';
@@ -308,13 +162,14 @@ class PagesController extends Controller {
             }
         } else {}
     }
-
+    
     public function actionCart() {
         if (isset($_SESSION['custId'])) {
             $this->removeFromCart();
+            
             $id = $_SESSION['custId'];
             
-            $cartList = OrderItems::find("custId = '$id'", OrderItems::tableName());
+            $cartList = OrderItems::find("custId = '$id' AND isSend = 'f'", OrderItems::tableName());
             $productList = array();
             $priceList = array();
             $ttPrice = 0;
@@ -328,11 +183,67 @@ class PagesController extends Controller {
                 array_push($priceList, $price);
                 $ttPrice += $price;
             }
-
+            
             $this->setParams('cart', $cartList);
             $this->setParams('prodInfo', $productList);
             $this->setParams('price', $priceList);
             $this->setParams('ttprice', $ttPrice);
+            $this->setParams('itemSend', false);
+            
+            if (isset($_POST['send'])) {
+                $this->setParams('itemSend', true);
+
+                $idC = $_SESSION['custId'];
+                $custInfo = Customers::find("custId = '$idC'", Customers::tableName());
+                $orderInfo = OrderItems::find("custId = '$idC'", OrderItems::tableName());
+                $idA = $custInfo[0]['addressId'];
+                
+                // Check if there is an adressId
+                if (!empty($idA)) {
+                    $addressInfo = Address::find("addressId = '$idA'", Address::tableName());
+                    $this->setParams('addressInfo', $addressInfo);
+                }
+
+                if (isset($_POST['address'])) {
+                    // Check if the address is valid.
+                    if (!empty($_POST['street'])
+                     && !empty($_POST['number'])
+                     && !empty($_POST['zip'])
+                     && !empty($_POST['city'])) {
+                         // Check if the address is already in the database and give back the id.
+                         $street = $_POST['street'];
+                         $number = $_POST['number'];
+                         $zip = $_POST['zip'];
+                         $city = $_POST['city'];
+
+                         $address = Address::find("street = '$street' AND number = '$number' AND zip = '$zip' AND city = '$city'", Address::tableName());
+                        var_dump($address);
+                         // → If not create a new entry in the database and get the id.
+                         // Ready to order!!
+                         /* try {
+                             for ($idx = 0; $idx < count($orderInfo); $idx++) {
+                                 $idI = $orderInfo[$idx]['itemId'];
+                                 
+                                 $sql1 = "INSERT INTO ". Orders::tableName() . " (itemId, addressId) VALUES ('$idI', '$idA');";
+                                 $stmt = $GLOBALS['db']->prepare($sql1);
+                                 $stmt->execute();
+         
+                                 $sql2 = "UPDATE " . OrderItems::tableName() . " SET isSend = 't' WHERE itemId = $idI;";
+                                 $stmt = $GLOBALS['db']->prepare($sql2);
+                                 $stmt->execute();
+         
+                                 header('Location: index.php?c=pages&a=homepage');
+                             }
+                         } catch (\PDOException $e) {
+                             echo '<div class="alert alert-danger">Bestellung fehlgeschlagen.</div>';
+                             echo 'Update fehlgeschlagen: ' . $e->getMessage();
+                         } */
+                     } else {
+                        echo '<div class="alert alert-danger">Empfangsadresse unvollständig.</div>';
+                     }
+                }
+            }
+
         } else {
             header('Location: index.php?c=pages&a=homepage');
         }
