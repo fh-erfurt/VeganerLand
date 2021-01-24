c6d2b0783d4b17e088173943e0372ce394f1e939
Molham Al-khodari <73823402+Molham321@users.noreply.github.com>
2021-01-24 19:01:24 +0100

    Update footer.php

--- assets/statics/footer.php
+++ assets/statics/footer.php
@@ -1,72 +1,75 @@
+<!-- Molham Al-khodari -->
 
 <link rel="stylesheet" href="<?=STYLESPATH.'footerStyle.css'?>">
 
-<!-- <header class="page-footer">
+<!-- 
+<header class="page-footer">
     <h1>Veganer Land</h1>
-</header> -->
+</header> 
+-->
 
 <!-- FOOTER START -->
 <div class="footer">
     <div class="contain">
-    <div class="col">
-        <h1>UNTERNEHMEN</h1>
-        <ul>
-        <li><a href="?c=pages&a=about">About</a></li>
-        <li><a href="?c=pages&a=about#team">Team</a></li>
-        <li><a href="?c=pages&a=about">Services</a></li>
-        <li><a href="?c=pages&a=about#impressum">Impressum</a></li>
-        <li><a href="?c=pages&a=about">Analytik</a></li>
-        <li><a href="?c=pages&a=about#email">Get in touch</a></li>
-        </ul>
-    </div>
-    <div class="col">
-        <h1>PRODUKTE</h1>
-        <ul>
-        <li><a href="?c=pages&a=about">About</a></li>
-        <li><a href="?c=pages&a=about#dashboard">Dashboard</a></li>
-        <li><a href="?c=pages&a=about#content">Content</a></li>
-        </ul>
-    </div>
-    <div class="col">
-        <h1>KONTEN</h1>
-        <ul>
-        <li><a href="?c=pages&a=about">About</a></li>
-        <li><a href="?c=pages&a=about">Mission</a></li>
-        <li><a href="?c=pages&a=about">Services</a></li>
-        <li><a href="?c=pages&a=about">Social</a></li>
-        <li><a href="?c=pages&a=about#email">Get in touch</a></li>
-        </ul>
-    </div>
-    <div class="col">
-        <h1>RESSOURCEN</h1>
-        <ul>
-        <li><a href="?c=pages&a=about">Webmail</a></li>
-        <li><a href="?c=pages&a=about">Redeem code</a></li>
-        <li><a href="?c=pages&a=about">WHOIS lookup</a></li>
-        <li><a href="?c=pages&a=about">Site map</a></li>
-        <li><a href="?c=pages&a=about">Web templates</a></li>
-        <li><a href="?c=pages&a=about#email">Email templates</a></li>
-        </ul>
-    </div>
-    <div class="col">
-        <h1>UNTERSTÜTZUNG</h1>
-        <ul>
-        <li><a href="?c=pages&a=about#email">Contact us</a></li>
-        <li><a href="?c=pages&a=about#email">E-Mail</a></li>
-        <li><a href="?c=pages&a=about#support">Support</a></li>
-        <li><a href="?c=pages&a=about">Web chat</a></li>
-        <li><a href="?c=pages&a=about">Open ticket</a></li>
-        </ul>
-    </div>
-    <div class="col social">  <!--das haben wir nicht also keine icons ?-->
-        <h1>SOZIAL</h1>
-        <ul>
-        <li><img src="?c=pages&a=about" alt="F" width="32" style="width: 32px;"></li>
-        <li><img src="?c=pages&a=about" alt="I" width="32" style="width: 32px;"></li>
-        <li><img src="?c=pages&a=about" alt="T" width="32" style="width: 32px;"></li>
-        </ul>
-    </div>
-    <div class="clearfix"></div>
+        <div class="col">
+            <h1>UNTERNEHMEN</h1>
+            <ul>
+            <li><a href="?c=pages&a=about">About</a></li>
+            <li><a href="?c=pages&a=about#team">Team</a></li>
+            <li><a href="?c=pages&a=about">Services</a></li>
+            <li><a href="?c=pages&a=about#impressum">Impressum</a></li>
+            <li><a href="?c=pages&a=about">Analytik</a></li>
+            <li><a href="?c=pages&a=about#email">Get in touch</a></li>
+            </ul>
+        </div>
+        <div class="col">
+            <h1>PRODUKTE</h1>
+            <ul>
+            <li><a href="?c=pages&a=about">About</a></li>
+            <li><a href="?c=pages&a=about#dashboard">Dashboard</a></li>
+            <li><a href="?c=pages&a=about#content">Content</a></li>
+            </ul>
+        </div>
+        <div class="col">
+            <h1>KONTEN</h1>
+            <ul>
+            <li><a href="?c=pages&a=about">About</a></li>
+            <li><a href="?c=pages&a=about">Mission</a></li>
+            <li><a href="?c=pages&a=about">Services</a></li>
+            <li><a href="?c=pages&a=about">Social</a></li>
+            <li><a href="?c=pages&a=about#email">Get in touch</a></li>
+            </ul>
+        </div>
+        <div class="col">
+            <h1>RESSOURCEN</h1>
+            <ul>
+            <li><a href="?c=pages&a=about">Webmail</a></li>
+            <li><a href="?c=pages&a=about">Redeem code</a></li>
+            <li><a href="?c=pages&a=about">WHOIS lookup</a></li>
+            <li><a href="?c=pages&a=about">Site map</a></li>
+            <li><a href="?c=pages&a=about">Web templates</a></li>
+            <li><a href="?c=pages&a=about#email">Email templates</a></li>
+            </ul>
+        </div>
+        <div class="col">
+            <h1>UNTERSTÜTZUNG</h1>
+            <ul>
+            <li><a href="?c=pages&a=about#email">Contact us</a></li>
+            <li><a href="?c=pages&a=about#email">E-Mail</a></li>
+            <li><a href="?c=pages&a=about#support">Support</a></li>
+            <li><a href="?c=pages&a=about">Web chat</a></li>
+            <li><a href="?c=pages&a=about">Open ticket</a></li>
+            </ul>
+        </div>
+        <div class="col social">  <!--das haben wir nicht also keine icons ?-->
+            <h1>SOZIAL</h1>
+            <ul>
+            <li><img src="?c=pages&a=about" alt="F" width="32" style="width: 32px;"></li>
+            <li><img src="?c=pages&a=about" alt="I" width="32" style="width: 32px;"></li>
+            <li><img src="?c=pages&a=about" alt="T" width="32" style="width: 32px;"></li>
+            </ul>
+        </div>
+        <div class="clearfix"></div>
     </div>
 </div>
 <!-- END OF FOOTER -->
