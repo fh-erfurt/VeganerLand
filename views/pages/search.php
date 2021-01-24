5ecf516a0178af838f99cdf2b929d353f0ab80fe
Molham Al-khodari <73823402+Molham321@users.noreply.github.com>
2021-01-24 18:53:47 +0100

    Update search.php

--- views/pages/search.php
+++ views/pages/search.php
@@ -1,11 +1,15 @@
 <div class="block-container">
     <ul class="cards">
-        <? $counter = 0;
-
-            if (empty($search)) {
+        <?
+            $counter = 0;
+            if (empty($search)) 
+            {
                 echo "<div class='alert alert-danger'>Es konnte nichts gefunden werden.</div>";
-            } else {
+            }
+            else 
+            {
                 include "include.php";
-            }?>
+            }
+        ?>
     </ul>
 </div>
