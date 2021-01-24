ea80a2419b16ae3abc55acc95591d5a094ee316f
Molham Al-khodari <73823402+Molham321@users.noreply.github.com>
2021-01-24 16:28:00 +0100

    Update include.php

--- views/pages/include.php
+++ views/pages/include.php
@@ -1,27 +1,35 @@
 <?
-// @author Jessica Eckardtsberg
-$pages = isset($_GET['a']) ? $_GET['a'] : '';
+    // @author Jessica Eckardtsberg, @author Molham Al-khodari
+
+    $pages = isset($_GET['a']) ? $_GET['a'] : '';
 ?>
 
 <div class="block-container">
     <ul class="cards">
-        <? $counter = 0;
-        while($counter < count($$pages)): 
-            if ($$pages[$counter]['cat'] === 'V' || $$pages[$counter]['cat'] === 'P' || $$pages[$counter]['cat'] === 'M') {
+        <? 
+            $counter = 0;
+            while($counter < count($$pages)): 
+
+            if ($$pages[$counter]['cat'] === 'V' || $$pages[$counter]['cat'] === 'P' || $$pages[$counter]['cat'] === 'M') 
+            {
                 $name = VEGETABLEPATH;
-            } else {
+            } 
+            else
+            {
                 $name = FRUITPATH;
-            }?>
+            }
+        ?>
             <li class="cards__item"> 
                 <div class="col">
                     <div class="card">
                         <div class="card-title"><?=$$pages[$counter]['descrip']?></div>
                         <img class='ard__image card__image--fence' src="<?=$name.$$pages[$counter]['descrip']?>.jpg" class="card-img-top" alt="Artikel">
                         <div class="card__text">
-                            <p>test product, hier eine tolle lange beschreibung</p>
+                            <p>test product, hier eine tolle lange beschreibung</p> <!--wird später aus dem datenbank gelesen-->
                             <hr>
                             <p><?=$$pages[$counter]['stdPrice']?></p>
                         </div>
+
                         <div class="card-footer">
                         <form class="card-footer" method="post">
                             <button class="btn btn--block card__btn" name="fav" type="submit" value="<?=$$pages[$counter]['prodId']?>"><?echo $$pages[$counter]['prodId']?></button>
@@ -32,7 +40,9 @@ $pages = isset($_GET['a']) ? $_GET['a'] : '';
                     </div>
                 </div>
             </li>
-        <?  $counter++;
-        endwhile; ?>
+        <?
+            $counter++;
+            endwhile; 
+        ?>
     </ul>
 </div>
