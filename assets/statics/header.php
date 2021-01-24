ef925130ff4bbd10f0e80854e74bd5d6ae6c437c
Molham Al-khodari <73823402+Molham321@users.noreply.github.com>
2021-01-24 19:03:20 +0100

    Update header.php

--- assets/statics/header.php
+++ assets/statics/header.php
@@ -1,7 +1,5 @@
 <!-- 
     //@author Molham Al-khodari
-    // 20.12.2020
-    // 00:00 Uhr
  -->
 
 <!Doctype html>
@@ -11,23 +9,30 @@
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <title><?php getTitle()?></title>
             <link rel="stylesheet" href="<?=STYLESPATH.'staticStyle.css'?>">
+
             <? if ($pageTitle == 'Sing Up') : ?> 
             <link rel='stylesheet' href ="<?=STYLESPATH.'registrationStyle.css'?>">
             <script type="text/javascript" src="assets/js/forms.js"></script>
+
             <? elseif ($pageTitle == 'Login' || $pageTitle == 'Passwort zurücksetzen'): ?>
             <link rel='stylesheet' href ="<?=STYLESPATH.'loginStyle.css'?>">
             <script type="text/javascript" src="assets/js/forms.js"></script>
+
             <? elseif ($pageTitle == 'Reset Password'): ?>
             <link rel='stylesheet' href ="<?=STYLESPATH.'loginStyle.css'?>">
+
             <? elseif ($pageTitle == 'Homepage'): ?>
-            <link rel='stylesheet' href ="<?=STYLESPATH.'homepageStyle.css'?>">  
+            <link rel='stylesheet' href ="<?=STYLESPATH.'homepageStyle.css'?>">
+
             <? elseif ($pageTitle == 'Einstellungen'): ?>
             <link rel="stylesheet" href="<?=STYLESPATH.'settingStyle.css'?>">
+
             <? elseif ($pageTitle == 'Über uns'): ?>
             <link rel="stylesheet" href="<?=STYLESPATH.'aboutStyle.css'?>">
+
             <? elseif ($pageTitle == 'Obst' || $pageTitle == 'Gemüse' || $pageTitle == 'Angebote' || $pageTitle == 'Suchergebnisse' || $pageTitle == 'Warenkorb'): ?>
             <link rel="stylesheet" href="<?=STYLESPATH.'fruitStyle.css'?>">
-            <? endif; ?>
-            
+
+            <? endif; ?>   
         </head>
         <body>
