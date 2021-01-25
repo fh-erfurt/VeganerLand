a1919c3d1b97dc34b0a454847353ec5fda688b5b
Jess0208 <73823283+Jess0208@users.noreply.github.com>
2021-01-25 10:09:49 +0100

    New include.php
    Couldn't change the path for the images → only one path for now.
    If not logged in the user can't see the buttons for "Warenkorb" and "Favorit".
    Also no double "$" anymore!

--- views/pages/include.php
+++ views/pages/include.php
@@ -1,48 +1,39 @@
 <?
     // @author Jessica Eckardtsberg, @author Molham Al-khodari
-
-    $pages = isset($_GET['a']) ? $_GET['a'] : '';
 ?>
 
 <div class="block-container">
-    <ul class="cards">
-        <? 
-            $counter = 0;
-            while($counter < count($$pages)): 
-
-            if ($$pages[$counter]['cat'] === 'V' || $$pages[$counter]['cat'] === 'P' || $$pages[$counter]['cat'] === 'M') 
-            {
-                $name = VEGETABLEPATH;
-            } 
-            else
-            {
-                $name = FRUITPATH;
-            }
-        ?>
+    <?
+    for ($idx = 0; $idx < count($products); $idx++) 
+    {
+        for ($prodidx = 0; $prodidx < count($products[$idx]); $prodidx++)
+        { ?>
             <li class="cards__item"> 
                 <div class="col">
                     <div class="card">
-                        <div class="card-title"><?=$$pages[$counter]['descrip']?></div>
-                        <img class='ard__image card__image--fence' src="<?=$name.$$pages[$counter]['descrip']?>.jpg" class="card-img-top" alt="Artikel">
+                        <div class="card-title"><?=strtr($products[$idx][$prodidx]["descrip"],"_"," ")?></div>
+                        <img class='ard__image card__image--fence' src="<?=IMAGEPATH.$products[$idx][$prodidx]["descrip"]?>.jpg" class="card-img-top" alt="Artikel">
                         <div class="card__text">
-                            <p>test product, hier eine tolle lange beschreibung</p> <!--wird später aus dem datenbank gelesen-->
+                            <p><? echo strtr($products[$idx][$prodidx]["comment"],"_"," ")?></p> <!--wird später aus dem datenbank gelesen-->
                             <hr>
-                            <p><?=$$pages[$counter]['stdPrice']?></p>
+                            <p><?=$products[$idx][$prodidx]['stdPrice']?></p>
                         </div>
 
                         <div class="card-footer">
+                        <?
+                        if (!empty($_SESSION['email']))
+                        { ?>
                         <form class="card-footer" method="post">
-                            <button class="btn btn--block card__btn" name="fav" type="submit" value="<?=$$pages[$counter]['prodId']?>"><?echo $$pages[$counter]['prodId']?></button>
-                            <button class="btn btn--block card__btn" name="submit" type="submit" value="<?=$$pages[$counter]['descrip']?>">In den Warenkorb</button>
+                            <button class="btn btn--block card__btn" name="fav" type="submit" value="<?=$products[$idx][$prodidx]['prodId']?>">Favorit</button>
+                            <button class="btn btn--block card__btn" name="submit" type="submit" value="<?=$products[$idx][$prodidx]['descrip']?>">In den Warenkorb</button>
                             <input name="qty" type="number">
                         </form>
+                        <? } ?>
                         </div>
                     </div>
                 </div>
             </li>
-        <?
-            $counter++;
-            endwhile; 
-        ?>
-    </ul>
+          <?
+        }
+    }?>
 </div>
