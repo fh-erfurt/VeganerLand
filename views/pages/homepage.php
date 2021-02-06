7b7c05a9e98c1d4ba568cf3987dd0488e5438590
Molham Al khodari <73823402+Molham321@users.noreply.github.com>
2021-02-06 13:40:29 +0100

    Update Homepage code style

--- views/pages/homepage.php
+++ views/pages/homepage.php
@@ -1,3 +1,10 @@
+
+<!-- 
+====================
+== Molham Al-khodari
+==================== 
+-->
+
 <!-- inform about the current situation -->
 <div class="note">
    <span>aus berührung wird Rührung</span> - wir sind online weiter hin für dich da!
@@ -8,7 +15,8 @@
     <h1>Willkommen bei <em>VeganerLand</em></h1>
     <p>veganer land ist eine Website zum Verkauf von Obst- und Gemüseprodukten.</p>
     <a href="?c=registration&a=registration">
-    <input type="submit" class="homepage-button" value="SIGN UP HERE">
+        <!-- <input type="submit" class="homepage-button" value="SIGN UP HERE"> -->
+        <button class="homepage-button">Hier Registrieren</button>
     </a>
 </section>
 
@@ -18,26 +26,26 @@
     
         <div class="block-fruit">
             <a href="?c=products&a=fruits">
-                <img src="assets/images/obst.jpg" alt="obst">
-                <p><button class="homepage-button" src="?c=pages&a=fruits">Obst</button></p>
+                <img src="assets/images/obst.jpg" alt="Obst">
+                <p><button class="homepage-button">Obst</button></p>
             </a>
         </div>
     
         <div class="block-vegetable">
             <a href="?c=products&a=vegetables">
-                <img src="assets/images/gemüse.jpg" alt="gemüse">
-                <p><button class="homepage-button" src="?c=pages&a=vegetables">Gemüse</button></p>
+                <img src="assets/images/gemüse.jpg" alt="Gemüse">
+                <p><button class="homepage-button">Gemüse</button></p>
             </a>
         </div>
     </div>
  </section>
 
 <!-- bargain block with photos and button -->
- <section class="angebot">
+ <section class="bargain">
  <div class="blockFull">
             <a href="?c=products&a=bargain">
-                <img src="assets/images/angebot.jpg" alt="angebot">
-                <p><button class="homepage-button" src="?a=angebote">Angebote</button></p>
+                <img src="assets/images/angebot.jpg" alt="Angebote">
+                <p><button class="homepage-button">Angebote</button></p>
             </a>
         </div>
  </section>
