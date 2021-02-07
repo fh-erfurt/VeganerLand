0dd74f55339241c1c2cf45dbe251a6134531248a
Molham Al-khodari <73823402+Molham321@users.noreply.github.com>
2021-02-07 10:34:42 +0100

    Update favorits.class.php

--- models/favorits.class.php
+++ models/favorits.class.php
@@ -9,5 +9,18 @@ class Favorits extends BaseModel
         'custID' => ['type' => BaseModel::TYPE_INT],
     ];
 
+    static public function ItemsFavorits ()
+    {
+        if(isset($_SESSION['custId']))
+        {
+            $custId = $_SESSION['custId'];
+            $sql = "SELECT COUNT(prodId) FROM favorits WHERE custId =".$custId;
+            $favoritsResult = $GLOBALS['db']->query($sql);
+            $favoritsItems = $favoritsResult->fetchColumn();
+            
+            return $favoritsItems;  // anzahl produkte im favorits list ;)
+        }
+    }
+
 }
 ?>
