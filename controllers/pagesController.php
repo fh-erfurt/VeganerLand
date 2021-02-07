989063a8a9c0f38693929fa714d33f4f47c98bd5
Molham Al-khodari <73823402+Molham321@users.noreply.github.com>
2021-02-07 10:18:23 +0100

    Update pagesController.php

--- controllers/pagesController.php
+++ controllers/pagesController.php
@@ -3,30 +3,30 @@
 // @author Molham Al-Khodari, @author Jessica Eckardtsberg
 // @version 1.0.0
 
-class PagesController extends Controller {
+class PagesController extends Controller
+{
 
     // public function actionIndex(){
     //     header('Location: index.php?c=pages&a=homepage');
     // }
     
-    public function actionHomepage() {
+    public function actionHomepage()
+    {
         // Here is nothing to do.
     }
 
-    public function actionLogin() 
+    public function actionLogin()
     {
-        if(isset($_SESSION['email']))
-        {
+        if (isset($_SESSION['email'])) {
             header('Location: ?c=pages&a=homepage');
         }
         
         // check if User coming from http post
     
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') 
-        {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $email          = $_POST['email'];
             $password       = $_POST['password'];
-            $hashedPassword = md5($password);  
+            $hashedPassword = md5($password);
     
             // check if the User Exist in Database
     
@@ -37,16 +37,13 @@ class PagesController extends Controller {
     
             // if Count > 0 This Mean The Database Cotanin Record About This Email
     
-            if ($count > 0)
-            {
+            if ($count > 0) {
                 $_SESSION['email'] = $email;                    // Register Session Email
                 $_SESSION['custId']= $row['custId'];            // Register Customer ID
                 $_SESSION['addressId']= $row['addressId'];      // Register Address ID
                 header('Location: ?c=pages&a=homepage');
                 exit();
-            }
-            else 
-            {
+            } else {
                 $status = "You Email or Password is incorrect";
                 viewError($status);
             }
@@ -57,11 +54,9 @@ class PagesController extends Controller {
     {
         $do = isset($_GET['do']) ? $_GET['do'] : '';
 
-        if ($do == 'identify') 
-        {
+        if ($do == 'identify') {
             // check is User coming from http post
-            if (isset($_POST['submit'])) 
-            {
+            if (isset($_POST['submit'])) {
                 $email = $_POST['email'];
         
                 // check if the User Exist in Database
@@ -72,8 +67,7 @@ class PagesController extends Controller {
                 $count = $stmt->rowCount();
 
                 // if Count > 0 This Mean The Database Cotanin Record About This Email
-                if ($count != 0) 
-                {
+                if ($count != 0) {
                     $tocken = generateRandomString(25);
                     $stmt = $GLOBALS['db']->prepare("UPDATE customers SET tocken = :tocken WHERE email = :email");
                     $stmt->bindParam(":tocken", $tocken, );
@@ -86,35 +80,27 @@ class PagesController extends Controller {
                     
                     // reset alternative password
 
-                    $file = fopen('controllers/reternPassword.txt', 'a+');
+                    $file = fopen('data/returnPassword.txt', 'a+');
                     fwrite($file, $txt.PHP_EOL);
                     fclose($file);
 
-                    mail($mailTo, $subject, $txt);  // macht Probleme aber egal wir habe kein mail server 
+                    //mail($mailTo, $subject, $txt);  // macht Probleme aber egal wir habe kein mail server
 
                     echo '<div class="alert alert-info">Email wurde versendet</div>';
-                } 
-                else 
-                {
+                } else {
                     viewError("There is no such Email");
                 }
             }
-        }
-        elseif ($do == 'setPassword') 
-        {
-            if (isset($_GET["tocken"])) 
-            {
+        } elseif ($do == 'setPassword') {
+            if (isset($_GET["tocken"])) {
                 $stmt = $GLOBALS['db']->prepare("SELECT * FROM customers WHERE tocken= :tocken LIMIT 1");
                 $stmt->bindParam(":tocken", $_GET['tocken']);
                 $stmt->execute();
                 $count = $stmt->rowCount();
             
-                if ($count != 0) 
-                {
-                    if (isset($_POST['submit'])) 
-                    {
-                        if ($_POST["password1"] == $_POST["password2"]) 
-                        {
+                if ($count != 0) {
+                    if (isset($_POST['submit'])) {
+                        if ($_POST["password1"] == $_POST["password2"]) {
                             $passwordHash = md5($_POST['password1']);
                             $stmt = $GLOBALS['db']->prepare('UPDATE customers SET password = :password, tocken = null WHERE tocken = :tocken');
                             $stmt->bindParam(':password', $passwordHash);
@@ -122,42 +108,32 @@ class PagesController extends Controller {
                             $stmt->execute();
 
                             header('Location: ?c=pages&a=login');
-                        } 
-                        else 
-                        {
+                        } else {
                             viewError("die Passwörte stimmen nicht überein");
                         }
                     }
-                }
-                else 
-                {
+                } else {
                     viewError("kein gültige tocken gesendet");
                 }
-            }
-            else 
-            {
+            } else {
                 viewError("kein gültige tocken gesendet");
             }
         }
-    }       
+    }
     
     public function actionSearch()
     {
         // Input whta to search in field (Name of fruit of veggie)
-        if (isset($_POST['submit'])) 
-        {
+        if (isset($_POST['submit'])) {
             $search = $_POST['search'];
             $result = array();
 
             $info = Products::find("descrip LIKE '%$search%'");
-            if (!empty($info))
-            {
+            if (!empty($info)) {
                 array_push($result, $info);
 
                 $this->setParams('products', $result);
-            }
-            else
-            {
+            } else {
                 $this->setParams('products', array());
                 echo "<div class='alert alert-danger'>Es konnte nichts gefunden werden.</div>";
             }
@@ -169,13 +145,12 @@ class PagesController extends Controller {
         // This is a static site. So nothing is to do, but we kind off need the method, I think.
     }
 
-    public function actionSetting() 
+    public function actionSetting()
     {
         // Customer can look into his given data and change his address, phone number and favorite products.
     
-            if (isset($_SESSION['email'])) 
-            {
-                $info = ['custId'   => null,
+        if (isset($_SESSION['email'])) {
+            $info = ['custId'   => null,
                         'firstName' => null,
                         'lastName'  => null,
                         'email'     => null,
@@ -186,33 +161,31 @@ class PagesController extends Controller {
                         'number'    => null,
                         'zip'       => null,
                         'city'      => null];
-                $email = $_SESSION['email'];
+            $email = $_SESSION['email'];
     
-                $custInfo = Customers::find("email = '$email'");
-                $info['custId'] = $custInfo[0]['custId'];
-                $info['firstName'] = $custInfo[0]['firstName'];
-                $info['lastName'] = $custInfo[0]['lastName'];
-                $info['email'] = $custInfo[0]['email'];
-                $info['phone'] = $custInfo[0]['phone'];
-                $info['password'] = $custInfo[0]['password'];
+            $custInfo = Customers::find("email = '$email'");
+            $info['custId'] = $custInfo[0]['custId'];
+            $info['firstName'] = $custInfo[0]['firstName'];
+            $info['lastName'] = $custInfo[0]['lastName'];
+            $info['email'] = $custInfo[0]['email'];
+            $info['phone'] = $custInfo[0]['phone'];
+            $info['password'] = $custInfo[0]['password'];
     
-                $addressId = $custInfo[0]['addressId'];
-                $info['addressId'] = $addressId;
+            $addressId = $custInfo[0]['addressId'];
+            $info['addressId'] = $addressId;
     
-                if (!empty($addressId)) 
-                {
-                    $addressInfo = Address::find("addressId = '$addressId'", Address::tableName());
-                    $info['street'] = $addressInfo[0]['street'];
-                    $info['number'] = $addressInfo[0]['number'];
-                    $info['zip'] = $addressInfo[0]['zip'];
-                    $info['city'] = $addressInfo[0]['city'];
-                }
+            if (!empty($addressId)) {
+                $addressInfo = Address::find("addressId = '$addressId'", Address::tableName());
+                $info['street'] = $addressInfo[0]['street'];
+                $info['number'] = $addressInfo[0]['number'];
+                $info['zip'] = $addressInfo[0]['zip'];
+                $info['city'] = $addressInfo[0]['city'];
+            }
     
-                $this->setParams('customerInfo', $info);
+            $this->setParams('customerInfo', $info);
     
-                if (isset($_POST['submit'])) 
-                {
-                    $newInfo = ['custId'    => $_POST['custId'],
+            if (isset($_POST['submit'])) {
+                $newInfo = ['custId'    => $_POST['custId'],
                                 'email'     => $_POST['email'],
                                 'password'  => $_POST['oldPassword'],
                                 'phone'     => $_POST['phone'],
@@ -221,72 +194,54 @@ class PagesController extends Controller {
                                 'number'    => $_POST['number'],
                                 'zip'       => $_POST['zip'],
                                 'city'      => $_POST['city']];
-                    $formErrors = 0;
-                    $noNewAddress = false;
+                $formErrors = 0;
+                $noNewAddress = false;
     
-                    if (!empty($_POST['newPassword'])) 
-                    {
-                        if (isPasswordSafe($_POST['newPassword'])) 
-                        {
-                            $newInfo['password'] = md5($_POST['newPassword']);
-                        } 
-                        else 
-                        {
-                            $formErrors++;
-                        }
-                    }
-    
-                    if (empty($newInfo['email'])) 
-                    {
+                if (!empty($_POST['newPassword'])) {
+                    if (isPasswordSafe($_POST['newPassword'])) {
+                        $newInfo['password'] = md5($_POST['newPassword']);
+                    } else {
                         $formErrors++;
-                    } 
-                    else 
-                    {
-                        // Checks if the email has been changed into another email already existing in the database.
-                        if (doesEmailExists($newInfo['email'])
-                        &&  $newInfo['email'] !== $email) 
-                        {
-                            $formErrors++;
-                        }
                     }
+                }
     
-                    // Checks if all fields are filled.
-                    if (!empty($newInfo['street'])
+                if (empty($newInfo['email'])) {
+                    $formErrors++;
+                } else {
+                    // Checks if the email has been changed into another email already existing in the database.
+                    if (doesEmailExists($newInfo['email'])
+                        &&  $newInfo['email'] !== $email) {
+                        $formErrors++;
+                    }
+                }
+    
+                // Checks if all fields are filled.
+                if (!empty($newInfo['street'])
                     &&  !empty($newInfo['number'])
                     &&  !empty($newInfo['zip'])
-                    &&  !empty($newInfo['city']))
-                    {
-    
-                        $street = $newInfo['street'];
-                        $number = $newInfo['number'];
-                        $zip = $newInfo['zip'];
-                        $city = $newInfo['city'];
-                        $addressInfo = Address::find("street = '$street' AND
+                    &&  !empty($newInfo['city'])) {
+                    $street = $newInfo['street'];
+                    $number = $newInfo['number'];
+                    $zip = $newInfo['zip'];
+                    $city = $newInfo['city'];
+                    $addressInfo = Address::find("street = '$street' AND
                                                       number = '$number' AND 
                                                       zip    = '$zip'    AND 
                                                       city   = '$city'");
-    
-                    } 
-                    else if (!empty($newInfo['street'])//Checks if only some fields are filled.
+                } elseif (!empty($newInfo['street'])//Checks if only some fields are filled.
                            ||  !empty($newInfo['number'])
                            ||  !empty($newInfo['zip'])
-                           ||  !empty($newInfo['city'])) 
-                    {
-                        $formErrors++;
-                    } 
-                    else 
-                    {
-                        $noNewAddress = true;
-                    }
+                           ||  !empty($newInfo['city'])) {
+                    $formErrors++;
+                } else {
+                    $noNewAddress = true;
+                }
     
-                    if ($formErrors !== 0) 
-                    {
-                        viewError("Update konnte nicht ausgeführt werden. Angaben waren unvollständig oder unzulässig.");
-                    } 
-                    else 
-                    {
-                        // Update the Database
-                        if (empty($addressInfo) && !$noNewAddress){ //Creats an new entry in sddress table if address doesn't exists there.
+                if ($formErrors !== 0) {
+                    viewError("Update konnte nicht ausgeführt werden. Angaben waren unvollständig oder unzulässig.");
+                } else {
+                    // Update the Database
+                        if (empty($addressInfo) && !$noNewAddress) { //Creats an new entry in sddress table if address doesn't exists there.
                             try {
                                 $sql1 = "INSERT INTO " . Address::tableName() . " (street, number, zip, city) 
                                          VALUES ('$street', '$number', '$zip', '$city');";
@@ -295,46 +250,40 @@ class PagesController extends Controller {
                                 $stmt->execute();
     
                                 $newInfo['addressId'] = $GLOBALS['db']->lastInsertId();
-                            } 
-                            catch (\PDOException $e)
-                            {
+                            } catch (\PDOException $e) {
                                 echo 'Fehlschlag: ' . $e->getMessage();
                             }
-                        } 
-                        else if (!$noNewAddress)
-                        {
+                        } elseif (!$noNewAddress) {
                             $newInfo['addressId'] = $addressInfo[0]['addressId'];
                         }
     
-                        // Update Customer Entry
-                        $email = $newInfo['email'];
-                        $phone = $newInfo['phone'];
-                        $password = $newInfo['password'];
-                        $addressId = $newInfo['addressId'];
-                        $id = $newInfo['custId'];
+                    // Update Customer Entry
+                    $email = $newInfo['email'];
+                    $phone = $newInfo['phone'];
+                    $password = $newInfo['password'];
+                    $addressId = $newInfo['addressId'];
+                    $id = $newInfo['custId'];
     
-                        try {
-                            $sql2 = "UPDATE " . Customers::tableName() . " SET 
+                    try {
+                        $sql2 = "UPDATE " . Customers::tableName() . " SET 
                                     email = '$email', phone = '$phone', password = '$password', addressId = '$addressId' 
                                     WHERE custId = $id;";
                             
-                            $stmt = $GLOBALS['db']->prepare($sql2);
-                            $stmt->execute();
-                        } 
-                        catch (\PDOException $e) 
-                        {
-                            echo 'Update fehlgeschlagen: ' . $e->getMessage();
-                        }
-                        header('Location: ?c=pages&a=setting');
-                        echo '<div class="alert alert-success">Update erfolgreich!</div>';
+                        $stmt = $GLOBALS['db']->prepare($sql2);
+                        $stmt->execute();
+                    } catch (\PDOException $e) {
+                        echo 'Update fehlgeschlagen: ' . $e->getMessage();
                     }
+                    header('Location: ?c=pages&a=setting');
+                    echo '<div class="alert alert-success">Update erfolgreich!</div>';
                 }
-            } 
-            else 
-            {
-                header('Location: ?c=pages&a=homepage');
-                viewError('Du bist nicht angemeldet! <a href="?c=pages&a=login">Anmelden</a>'); // das macht hier kein sinn!
             }
+        } else {
+            header('Location: ?c=pages&a=homepage');
+            viewError('Du bist nicht angemeldet! <a href="?c=pages&a=login">Anmelden</a>'); // das macht hier kein sinn!
+        }
+
+        $this->viewFavorites();
     }
     
     public function actionLogout()
@@ -343,6 +292,42 @@ class PagesController extends Controller {
         $this->setParams('password', null);
 
         header('Location: index.php?c=pages&a=homepage');
+    }
+
+    public function viewFavorites()
+    {
+            //$this->removeFromCart();
+        
+            // $custId = $_SESSION['custId'];
+
+            // $sql = 'SELECT * from favorits WHERE custId = ' . $custId;
+            // $result = $GLOBALS['db']->query($sql)->fetchAll();
+
+            // print_r($result);
+
+            $id = $_SESSION['custId'];
+        
+            $favoritsList = Favorits::find("custId = '$id'");
+            var_dump($favoritsList);
+
+            $productList = array();
+
+            for ($idx = 0; $idx < count($favoritsList); $idx++) 
+            {
+                $id = $favoritsList[$idx]['prodId'];
+                $productInfo = Products::find("prodId = '$id'");
+                array_push($productList, $productInfo);
+
+                $price =  $productList[$idx][0]['stdPrice'];
+                //array_push($priceList, $price);
+            }
+        
+            $this->setParams('cart', $favoritsList);
+            $this->setParams('prodInfo', $productList);
+            $this->setParams('price', $priceList);
+
+        
+
     }
 }
 ?>
