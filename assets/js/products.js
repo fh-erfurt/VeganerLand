4659b35e1cb1017428bf022187e3566b6fee05c5
Jess0208 <73823283+Jess0208@users.noreply.github.com>
2021-02-21 15:59:28 +0100

    Update products.js

--- assets/js/products.js
+++ assets/js/products.js
@@ -43,3 +43,148 @@ function sendProductData(event, formData = 'form') {
 
     request.send(requestForm);
 }
+
+function changeDisplay(index, tag, filter, call = 0) {
+    var products = document.getElementById('products');
+    var li = products.getElementsByTagName('li');
+
+    if (call == filter.length) {
+        li[index].style.display = "inline-block";
+    } else {
+        var currentTag = tag[call];
+        var check = li[index].getElementsByTagName('p')[currentTag].textContent;
+        if (check.indexOf(filter[call]) == -1) {
+            li[index].style.display = "none";
+            alert("Nichts da!");
+        } else {
+            call++;
+            changeDisplay(index, tag, filter, call);
+        }
+    }
+}
+
+function reload(event) {
+    event.stopPropagation();
+    event.preventDefault();
+
+    var form = document.getElementById('form');
+    var bioFilter = [form[0].checked, 0, "Bio"];
+    var regionalFilter = [form[1].checked, 0, "Regional"];
+    var priceFilter = [form[2].value, 1, form[2].value];
+    var weightFilter = [form[3].value, 0, form[3].value];
+
+    var contentList = document.getElementById('products');
+    var content = contentList.getElementsByTagName('li');
+    var i = 0;
+
+    switch (true) {
+        case bioFilter[0] && regionalFilter[0] && priceFilter[0] != "" && weightFilter[0] != "":
+            var tagList = [bioFilter[1], regionalFilter[1], priceFilter[1], weightFilter[1]];
+            var filterList = [bioFilter[2], regionalFilter[2], priceFilter[2], weightFilter[2]];
+            for (i; i < content.length; i++) {
+                changeDisplay(i, tagList, filterList);
+            }
+            break;
+        case bioFilter[0] && regionalFilter[0] && priceFilter[0] != "":
+            var tagList = [bioFilter[1], regionalFilter[1], priceFilter[1]];
+            var filterList = [bioFilter[2], regionalFilter[2], priceFilter[2]];
+            for (i; i < content.length; i++) {
+                changeDisplay(i, tagList, filterList);
+            }
+            break;
+        case bioFilter[0] && regionalFilter[0] && weightFilter[0] != "":
+            var tagList = [bioFilter[1], regionalFilter[1], weightFilter[1]];
+            var filterList = [bioFilter[2], regionalFilter[2], weightFilter[2]];
+            for (i; i < content.length; i++) {
+                changeDisplay(i, tagList, filterList);
+            }
+            break;
+        case bioFilter[0] && priceFilter[0] != "" && weightFilter[0] != "":
+            var tagList = [bioFilter[1], priceFilter[1], weightFilter[1]];
+            var filterList = [bioFilter[2], priceFilter[2], weightFilter[2]];
+            for (i; i < content.length; i++) {
+                changeDisplay(i, tagList, filterList);
+            }
+            break;
+        case regionalFilter[0] && priceFilter[0] != "" && weightFilter[0] != "":
+            var tagList = [regionalFilter[1], priceFilter[1], weightFilter[1]];
+            var filterList = [regionalFilter[2], priceFilter[2], weightFilter[2]];
+            for (i; i < content.length; i++) {
+                changeDisplay(i, tagList, filterList);
+            }
+            break;
+        case bioFilter[0] && regionalFilter[0]:
+            var tagList = [bioFilter[1], regionalFilter[1]];
+            var filterList = [bioFilter[2], regionalFilter[2]];
+            for (i; i < content.length; i++) {
+                changeDisplay(i, tagList, filterList);
+            }
+            break;
+        case bioFilter[0] && priceFilter[0] != "":
+            var tagList = [bioFilter[1], priceFilter[1]];
+            var filterList = [bioFilter[2], priceFilter[2]];
+            for (i; i < content.length; i++) {
+                changeDisplay(i, tagList, filterList);
+            }
+            break;
+        case bioFilter[0] && weightFilter[0] != "":
+            var tagList = [bioFilter[1], weightFilter[1]];
+            var filterList = [bioFilter[2], weightFilter[2]];
+            for (i; i < content.length; i++) {
+                changeDisplay(i, tagList, filterList);
+            }
+            break;
+        case regionalFilter[0] && priceFilter[0] != "":
+            var tagList = [regionalFilter[1], priceFilter[1]];
+            var filterList = [regionalFilter[2], priceFilter[2]];
+            for (i; i < content.length; i++) {
+                changeDisplay(i, tagList, filterList);
+            }
+            break;
+        case regionalFilter[0] && weightFilter[0] != "":
+            var tagList = [regionalFilter[1], weightFilter[1]];
+            var filterList = [regionalFilter[2], weightFilter[2]];
+            for (i; i < content.length; i++) {
+                changeDisplay(i, tagList, filterList);
+            }
+            break;
+        case priceFilter[0] != "" && weightFilter[0] != "":
+            var tagList = [priceFilter[1], weightFilter[1]];
+            var filterList = [priceFilter[2], weightFilter[2]];
+            for (i; i < content.length; i++) {
+                changeDisplay(i, tagList, filterList);
+            }
+            break;
+        case bioFilter[0]:
+            var tagList = [bioFilter[1]];
+            var filterList = [bioFilter[2]];
+            for (i; i < content.length; i++) {
+                changeDisplay(i, tagList, filterList);
+            }
+            break;
+        case regionalFilter[0]:
+            var tagList = [regionalFilter[1]];
+            var filterList = [regionalFilter[2]];
+            for (i; i < content.length; i++) {
+                changeDisplay(i, tagList, filterList);
+            }
+            break;
+        case priceFilter[0] != "":
+            var tagList = [priceFilter[1]];
+            var filterList = [priceFilter[2]];
+            for (i; i < content.length; i++) {
+                changeDisplay(i, tagList, filterList);
+            }
+            break;
+        case weightFilter[0] != "":
+            var tagList = [weightFilter[1]];
+            var filterList = [weightFilter[2]];
+            for (i; i < content.length; i++) {
+                changeDisplay(i, tagList, filterList);
+            }
+            break;
+        default:
+            location.reload();
+            break;
+    }
+}
