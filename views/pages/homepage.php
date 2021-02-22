6543b18d497de34a1d1cb7ae0a8d77ce67728c4c
Jess0208 <73823283+Jess0208@users.noreply.github.com>
2021-02-22 14:27:26 +0100

    Update homepage.php

--- views/pages/homepage.php
+++ views/pages/homepage.php
@@ -8,16 +8,16 @@
 
 <!-- inform about the current situation -->
 <div class="note">
-   <span>aus berührung wird Rührung</span> - wir sind online weiter hin für dich da!
+   <span>Aus Berührung wird Rührung</span> - Wir sind online weiter hin für dich da.
 </div>
 
 <!-- welcome phrase with brief description of the page -->
 <section class="header">
     <h1>Willkommen bei <em>VeganerLand</em></h1>
-    <p>veganer land ist eine Website zum Verkauf von Obst- und Gemüseprodukten.</p>
-    <a href="?c=registration&a=registration" title="go to Registration page">
+    <p>VeganerLand ist eine Website zum Verkauf von Obst- und Gemüseprodukten.</p>
+    <a href="?c=registration&a=registration" title="Konto erstellen">
         <!-- <input type="submit" class="homepage-button" value="SIGN UP HERE"> -->
-        <button class="homepage-button">Hier Registrieren</button>
+        <button class="homepage-button">Konto erstellen</button>
     </a>
 </section>
 
@@ -26,14 +26,14 @@
     <div class="rowBlock">
     
         <div class="block-fruit">
-            <a href="?c=products&a=fruits" title="go to fruit page">
+            <a href="?c=products&a=fruits" title="Produkte - Obst">
                 <img src="assets/images/obst.jpg" alt="Obst">
                 <p><button class="homepage-button">Obst</button></p>
             </a>
         </div>
     
         <div class="block-vegetable">
-            <a href="?c=products&a=vegetables" title="go to vegetable page">
+            <a href="?c=products&a=vegetables" title="Produkte - Gemüse">
                 <img src="assets/images/gemüse.jpg" alt="Gemüse">
                 <p><button class="homepage-button">Gemüse</button></p>
             </a>
@@ -44,7 +44,7 @@
 <!-- bargain block with photos and button -->
  <section class="bargain">
  <div class="blockFull">
-            <a href="?c=products&a=bargain" title="go to bargain page">
+            <a href="?c=products&a=bargain" title="Produkte - Angebote">
                 <img src="assets/images/angebot.jpg" alt="Angebote">
                 <p><button class="homepage-button">Angebote</button></p>
             </a>
