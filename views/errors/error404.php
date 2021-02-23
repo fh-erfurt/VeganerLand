a5ce6d0a550f09fb0a042c25885670027f7653a3
Molham Al-khodari <73823402+Molham321@users.noreply.github.com>
2021-02-23 16:45:58 +0100

    add comments

--- views/errors/error404.php
+++ views/errors/error404.php
@@ -1,16 +1,23 @@
- <div class="container">
-    <head>
+<!-- 
+====================
+== Mahmoud Mater
+== Molham Al-khodari
+====================
+-->
+
+<div class="container">
+    <head>  <!-- static error photo -->
     <img src="assets/images/error.png" alt="Error404">
     </head>
     <div class="container">
-        <div class="error-message">
+        <div class="error-message"> <!--dynamic error message-->
             <?=$errorMessage?>
         </div>
 
-        <div class="webmaster">
+        <div class="webmaster"> <!--report a problem-->
             Kontaktieren Sie uns <a href="mailto:molhamalkhodari@gmail.com">Email schreiben</a>
         </div>
-        <div>
+        <div> <!--back to homepage-->
         <a href="?c=pages&a=homepage">Gehen Sie zu unserer Startseite zurück.</a>
         </div>
     </div>
