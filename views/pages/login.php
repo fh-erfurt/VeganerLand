a0e4b20404c8671679bb8a78ce6b180277b71024
Molham Al-khodari <73823402+Molham321@users.noreply.github.com>
2021-02-23 17:00:21 +0100

    comments

--- views/pages/login.php
+++ views/pages/login.php
@@ -12,15 +12,13 @@
             <p>Melden Sie sich hier mit Ihrem Benutzernamen und Passwort an</p>
         </header>
 
-        <!--inputs-->
-        <div class="field-set">
+        <div class="field-set"> <!--inputs-->
             <input class="form-input" id="email" type="email" name="email" placeholder="Email" required> <br>
             <input class="form-input" id="password" type="password" name="password" placeholder="Password" required> <br>
             <button class="login" id="submit" name="submit" type="submit"> Login </button>
         </div>
 
-        <!--Forgot Password and Sign Up  -->
-        <div class="other">
+        <div class="other"> <!--Forgot Password and Sign Up  -->
                <button class="submits"> <a href="?c=pages&a=resetPassword&do=identify" title="Passwort zurücksetzen"> Passwort vergessen </a> </button>
                <button class="submits"><a href="?c=registration&a=registration" title="Konto erstellen">Registrieren</a> </button>
         </div>
