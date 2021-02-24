731be21917de323b1783ca4d1664f3faaebf22e0
Jess0208 <73823283+Jess0208@users.noreply.github.com>
2021-02-24 10:19:51 +0100

    Comments

--- models/category.class.php
+++ models/category.class.php
@@ -10,12 +10,13 @@ class Category extends BaseModel
         'name'      => ['type' => BaseModel::TYPE_STRING, 'min' => 2, 'max' => 45], 
     ];
 
-    public static function getCategoryName($mainCat)
+    public static function getCategoryName($mainCat) //$mainCat is either 'fruits' or 'vegetables'
     {
-        $list = Category::find("descrip LIKE '$mainCat%'");
+        $list = Category::find("descrip LIKE '$mainCat%'"); //Finds the relevant information in the database.
         $result = array();
         for ($idx = 0; $idx < count($list); $idx++)
         {
+            //Pushes the name and the last part of the description in $results.
             array_push($result, array($list[$idx]['name'], ltrim(strpbrk($list[$idx]['descrip'],"_"),"_")));
         }
 
